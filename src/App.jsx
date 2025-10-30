3d6bb49c4b9294599b8a45c818766738991c5784
Mohd Ajmal Raza <mohdajmalraza92786@gmail.com>
2025-10-30 21:36:15 +0530

    chore: render AddHotelForm component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import AddHotelForm from "./components/AddHotelForm";
 import Hotels from "./components/Hotels";
 import HotelByTitle from "./components/HotelByTitle";
 import "./App.css"
@@ -6,6 +7,7 @@ export default function App() {
   return (
     <>
       <main>
+        <AddHotelForm/>
         <Hotels />
         <HotelByTitle title="Sunset Resort" />
       </main>
